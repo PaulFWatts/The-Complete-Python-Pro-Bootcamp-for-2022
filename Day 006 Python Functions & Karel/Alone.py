590f80ba70a9651bfa83893083a97d4c849369fe
Paul Watts <paul.watts@me.com>
2022-06-23 19:19:59 +1000

    Partial complete Day 6

---
+++ Day 006 Python Functions & Karel/Alone.py
@@ -0,0 +1,22 @@
+# Goto https://reeborg.ca/reeborg.html to use Karel
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# Draw Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
