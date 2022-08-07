eca8d3c92a68560cf4bb34afbae1a630846fe1d7
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2022-08-07 14:59:37 +1000

    Complete Day 17

---
+++ Day 017 Benefits of OOP and the Quiz Project/examples.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, user_id, username):  # initialise attributes
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, other_user):
+        self.following += 1
+        other_user.followers += 1
+
+
+print("\033[2J\033[1;1H")  # clear screen
+user_1: User = User("001", "Paul")
+user_2: User = User("002", "John")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.followers)
+print(user_2.following)
--- Day 017 Benefits of OOP and the Quiz Project/main.py
+++ Day 017 Benefits of OOP and the Quiz Project/main.py
@@ -1,21 +1,21 @@
-class User:
-    def __init__(self, user_id, username):  # initialise attributes
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
 
-    def follow(self, other_user):
-        self.following += 1
-        other_user.followers += 1
+print("\033[2J\033[1;1H")  # clear screen
 
+# Create a QuizBrain object with the question_data list
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user_1: User = User("001", "Paul")
-user_2: User = User("002", "John")
+quiz = QuizBrain(question_bank)  # initialise the QuizBrain class
 
-user_1.follow(user_2)
-print(user_1.followers)
-print(user_1.following)
+while quiz.still_has_questions():  # while there are still questions left to ask
+    quiz.next_question()  # ask the next question
 
-print(user_2.followers)
-print(user_2.following)
+print("You've finished the quiz!")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
